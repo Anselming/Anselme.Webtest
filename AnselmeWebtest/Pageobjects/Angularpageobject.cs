9f0bbab6e92aebeee9b4fba6bc8dcdb63576640a
Thiago Anselme <anselme.thiago@gmail.com>
2020-01-20 11:46:17 -0300

    Now, solutions test the website https://angularjs.org/. With selenium on .NET Core 3 and XUnit we can test a site works with AngularJS (versions 1.x) sucessfully.

---
+++ AnselmeWebtest/Pageobjects/Angularpageobject.cs
@@ -0,0 +1,35 @@
+﻿using AnselmeWebtest.Structure;
+using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnselmeWebtest.Pageobjects
+{
+    internal class AngularPageobject : Pageobject
+    {
+        public AngularPageobject(IConfiguration configuration, Browser browser, bool showBrowser = false) 
+            : base(configuration, browser, showBrowser)
+        {
+        }
+
+        public void ChangeHello(string inputText)
+        {
+            By inputSearch = By.CssSelector(".ng-pristine:nth-child(2)");
+            Driver.SetText(inputSearch, inputText);
+
+            By helloLocation = By.XPath("//div[2]/div/h1");
+            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(Timeout));
+            wait.Until(d => d.FindElement(helloLocation) != null);
+        }
+
+        public string GetHello()
+        {
+            By hello = By.XPath("//div[2]/div/h1");
+            return Driver.GetText(hello);
+        }
+
+    }
+}
---
+++ AnselmeWebtest/Webtests/AngularWebtest.cs
@@ -0,0 +1,52 @@
+﻿using AnselmeWebtest.Pageobjects;
+using AnselmeWebtest.Structure;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace AnselmeWebtest.Webtests
+{
+    public class AngularWebteste
+    {
+        IConfiguration _configuration;
+        public AngularWebteste()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            _configuration = builder.Build();
+
+            // Set Brazilian Culture
+            var culture = new CultureInfo("pt-BR");
+            CultureInfo.DefaultThreadCurrentCulture = culture;
+            CultureInfo.DefaultThreadCurrentUICulture = culture;
+        }
+
+        [Theory]
+        [InlineData(Browser.Chrome, "John")]
+        [InlineData(Browser.Chrome, "Baltmor")]
+        [InlineData(Browser.Chrome, "Smart")]
+        [InlineData(Browser.Firefox, "Splash")]
+        [InlineData(Browser.Firefox, "Fast")]
+        [InlineData(Browser.Firefox, "Hospital")]
+        public void HelloInAngular(Browser browser, string text)
+        {
+            using (AngularPageobject pageobject = new AngularPageobject(_configuration, browser, true))
+            {
+                string actual = string.Empty;
+                string expected = text;
+
+                pageobject.LoadPage("https://angularjs.org/");
+                pageobject.ChangeHello(text);
+                actual = pageobject.GetHello();
+                expected = $"Hello {expected}!";
+
+                Assert.Equal(expected, actual);
+            }
+        }
+    }
+}
